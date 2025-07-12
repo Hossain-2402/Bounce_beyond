d567cbeb8c8a14015b495f429e8942a9425b3f25
Hossain-2402 <58787306+Hossain-2402@users.noreply.github.com>
2025-07-12 20:32:13 +0600

    Update iMain.cpp

--- iMain.cpp
+++ iMain.cpp
@@ -8,27 +8,28 @@ int vh = 5;
 int vw = 10;
 
 // Images
-int landing_page_background_image;
-int button_image;
-int ball_image;
-int play_screen_background_image;
+	int landing_page_background_image;
+	int button_image;
+	int ball_image;
+	int play_screen_background_image;
+	char ball_images[3][100] = { "ball_image_10.png", "ball_image_2.png", "ball_image_8.png",};
 
 // Toggle buttons
-int play_button_clicked = 0;
-int levels_button_clicked = 0;
-int quit_button_clicked = 0;
+	int play_button_clicked = 0;
+	int levels_button_clicked = 0;
+	int quit_button_clicked = 0;
 
 // Positions
-double x_of_play_screen_background = 0;
-double ball_y = 23 * vh;
-double rotating_angle = 0;
+	double x_of_play_screen_background = 0;
+	double ball_y = 23 * vh;
+	double rotating_angle = 0;
 
 // Variables
 bool is_animating = false;
 int frame_counter;
 const double jumpHeight = 100; // Target jump height in pixels
 double current_rotation_angle;
-char image[100] = "ball_image_1.png";
+int ball_img_index = 0;
 
 void show_play_screen() {
 	levels_button_clicked = 0;
@@ -37,9 +38,6 @@ void show_play_screen() {
 	for (int i = 0; i < 5; i++) {
 		iShowImage((x_of_play_screen_background + i * 100) * vw, 0, 100 * vw, 100 * vh, play_screen_background_image);
 	}
-
-	iSetColor(255, 255, 255);
-	iText(0, 80 * vh, "Play Screen");
 
 	iRotate((20 * vw + 12.5 * vh), 35.5 * vh, rotating_angle);
 	iShowImage(20 * vw, ball_y, 25 * vh, 25 * vh, ball_image);
@@ -58,19 +56,24 @@ void show_levels_screen() {
 }
 
 void create_landing_page() {
-	iShowImage(0, 0, 100 * vw, 100 * vh, landing_page_background_image);
+	iShowImage(0, 0, 100 * vw, 100 * vh, play_screen_background_image);
 
-	iShowImage(30 * vw, 50 * vh, 40 * vw, 40 * vh, button_image);
+	iShowImage(30 * vw, 65 * vh, 40 * vw, 40 * vh, button_image);
 	iSetColor(0, 0, 0);
-	iText(47.5 * vw, 70 * vh, "Play", GLUT_BITMAP_8_BY_13);
+	iText(47.5 * vw, 85 * vh, "Play", GLUT_BITMAP_8_BY_13);
 
-	iShowImage(30 * vw, 30 * vh, 40 * vw, 40 * vh, button_image);
-	iSetColor(0, 0, 0);
-	iText(46 * vw, 50 * vh, "Levels");
 
-	iShowImage(30 * vw, 10 * vh, 40 * vw, 40 * vh, button_image);
+	iShowImage(30 * vw, 45 * vh, 40 * vw, 40 * vh, button_image);
 	iSetColor(0, 0, 0);
-	iText(47.5 * vw, 30 * vh, "Quit");
+	iText(46 * vw, 65 * vh, "Levels");
+
+	iShowImage(30 * vw, 25 * vh, 40 * vw, 40 * vh, button_image);
+	iSetColor(0, 0, 0);
+	iText(46 * vw, 45 * vh, "Credits");
+
+	iShowImage(30 * vw, 5 * vh, 40 * vw, 40 * vh, button_image);
+	iSetColor(0, 0, 0);
+	iText(47.5 * vw, 25 * vh, "Quit");
 
 	if (play_button_clicked == 1) show_play_screen();
 	if (levels_button_clicked == 1) show_levels_screen();
@@ -91,14 +94,19 @@ void iPassiveMouseMove(int mx, int my) {
 }
 
 void iMouse(int button, int state, int mx, int my) {
+	printf("%d, %d \n", mx/vw,my/vh);
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 65 * vh && my <= 76 * vh)) {
+		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 80 * vh && my <= 90 * vh)) {
 			play_button_clicked = 1;
 		}
-		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 45 * vh && my <= 55 * vh)) {
+		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 60 * vh && my <= 70 * vh)) {
 			levels_button_clicked = 1;
 		}
-		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 25 * vh && my <= 35 * vh)) {
+
+		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 40 * vh && my <= 50 * vh)) {
+			//levels_button_clicked = 1;
+		}
+		if ((mx >= 30 * vw && mx <= 70 * vw) && (my >= 20 * vh && my <= 30 * vh)) {
 			quit_button_clicked = 1;
 		}
 	}
@@ -109,14 +117,14 @@ void iMouse(int button, int state, int mx, int my) {
 
 void iKeyboard(unsigned char key) {
 	if (key == 'd') {
-		rotating_angle -= 15;
+		if(!is_animating) rotating_angle -= 15;
 		current_rotation_angle = rotating_angle;
 
 		if (x_of_play_screen_background <= -400) x_of_play_screen_background = 0;
 		else x_of_play_screen_background -= 1;
 	}
 	if (key == 'a') {
-		rotating_angle += 15;
+		if (!is_animating) rotating_angle += 15;
 		current_rotation_angle = rotating_angle;
 
 
@@ -129,7 +137,11 @@ void iKeyboard(unsigned char key) {
 		frame_counter = 0;
 	}
 	if (key == 'c') {
-		strcpy_s(image,"ball_image_1.png");
+
+		if (ball_img_index >= 2) ball_img_index = 0;
+		else ball_img_index++;
+		ball_image = iLoadImage(ball_images[ball_img_index]);
+
 	}
 }
 
@@ -138,24 +150,26 @@ void iTimer() {
 		frame_counter++;
 		rotating_angle = 0;
 
-		// Move up for the first 60 frames (1 second)
+		
 		if (frame_counter <= 60) {
-			ball_y += (jumpHeight / 60); // Move up by approximately 1.67 pixels per frame
+			ball_y += ((50 * vh) / 60);
 		}
-		// Move down for the next 60 frames (1 second)
+		
 		else if (frame_counter > 60 && frame_counter <= 120) {
-			ball_y -= (jumpHeight / 60); // Move down by approximately 1.67 pixels per frame
+			if (ball_y <= 23 * vh) ball_y = 23 * vh;
+			else ball_y -= ((50 * vh) / 60);
+
 		}
 
-		// Reset when animation completes (2 seconds = 120 frames)
+		
 		if (frame_counter >= 120) {
-			ball_y = 23 * vh; // Reset to starting position
+			ball_y = 23 * vh;
 			is_animating = false;
 			frame_counter = 0;
 			rotating_angle = current_rotation_angle;
 		}
 	}
-	iSetTimer(1000 / 60, iTimer); // 60 FPS
+	iSetTimer(50000 / 60, iTimer);
 }
 
 void iSpecialKeyboard(unsigned char key) {
@@ -176,7 +190,7 @@ int main() {
 	landing_page_background_image = iLoadImage("background_image.png");
 	button_image = iLoadImage("button_image.png");
 	play_screen_background_image = iLoadImage("image (2).png");
-	ball_image = iLoadImage(image);
+	ball_image = iLoadImage(ball_images[0]);
 
 	iSetTimer(1000 / 60, iTimer); // Initialize timer for 60 FPS
 	iStart();
