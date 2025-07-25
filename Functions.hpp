525daeb3b1c6d0d9195b812733445323990f8900
Hossain-2402 <58787306+Hossain-2402@users.noreply.github.com>
2025-07-26 02:00:06 +0600

    Update Functions.hpp

--- Functions.hpp
+++ Functions.hpp
@@ -2,8 +2,7 @@
 #define FUNCTIONS_H
 #include "Variables.hpp"
 
-
-
+	// hover effects
 	void enlarge_play()
 	{
 		iShowImage(25 * vw, 60 * vh, 50 * vw, 50 * vh, button_image);
@@ -29,23 +28,49 @@
 		iText(47.5 * vw, 25 * vh, "QUIT", GLUT_BITMAP_HELVETICA_18);
 	}
 
-
-
+	// the game screen
+	void make_background(){
+		for (int i = 0; i < 5; i++) {
+			iShowImage((x_of_play_screen_background + i * 100) * vw, 0, 100 * vw, 100 * vh, play_screen_background_image);
+		}
+	}
+	void make_ball(){
+		//blast before level failed
+		if (show_blast && (blast_timer < blast_duration)) {
+			iShowImage(ball_x, ball_y, 25 * vh, 25 * vh, blast_image);
+		}
+		else if (show_blast && blast_timer >= blast_duration) {
+			level_failed = 1;
+			show_blast = false; // prevent looping
+		}
+		else{
+			//draw and rotate ball sprite
+			iRotate((ball_x + 7.5 * vh), (ball_y + 7.5 * vh), rotating_angle);
+			iShowImage(ball_x, ball_y, 15 * vh, 15 * vh, ball_image);
+			iUnRotate();
+		}
+	}
 	void make_obstacles(){
 		iShowImage(OBSTACLE_X*vw, OBSTACLE_Y * vh, 20 * vw, 20 * vh, obstacle_image_4);
 		iShowImage(SPIKE_X*vw, 24 * vh, 37.5 * vw, 12.5 * vh, spike_image);
 	}
-	void change_y_of_obstacle(){
-		OBSTACLE_Y = rand() % 21 + 40;
+	void make_coins(){
+		for (int i = 0; i < 50; i++){
+			double x = x_of_coin + i * 15;
+			int y;
+
+			if (x >= (OBSTACLE_X-10) && x <= OBSTACLE_X + 23) y = -50;
+			else y = 30;
+
+			iShowImage(x*vw, y*vh, coins[i].width * vh, coins[i].height * vh, coins[i].coin_image);
+		}
 	}
 	void move_obstacle(int moving_direction) { // moving_direction = 1 (moving RIGHT) moving_direction = -1 (moving LEFT)
 		if (OBSTACLE_X <= -20) {
 			OBSTACLE_X = 110;
-			change_y_of_obstacle();
 		}
 		else if (OBSTACLE_X >= 110) {
 			OBSTACLE_X = -20;
-			change_y_of_obstacle();
 		}
 		OBSTACLE_X += (moving_direction)* 1;
 
@@ -62,9 +87,21 @@
 
 
 	}
+
+	// moving actions
+	void change_y_of_obstacle(){
+		//OBSTACLE_Y = rand() % 21 + 40;
+		counter++;
+		if (counter <= 15) OBSTACLE_Y++;
+		else if (counter >= 15 && counter <= 30) OBSTACLE_Y--;
+		else{
+			counter = 0;
+		}
+
+	}
 	void jump_on_obstacle(){
-		if ((OBSTACLE_X >= 4 && OBSTACLE_X <= 25) && ball_y > 27*vh) GROUND_Y = (OBSTACLE_Y + 12) * vh;
-		else GROUND_Y = 27 * vh;
+		if ((OBSTACLE_X >= 4 && OBSTACLE_X <= 25) && ball_y > OBSTACLE_Y * vh) GROUND_Y = (OBSTACLE_Y + 12) * vh;
+		else GROUND_Y = 28 * vh;
 
 	}
 	void show_level_failed_screen(){
@@ -74,7 +111,7 @@
 
 	}
 	void ball_hit_spike(){
-		if ((SPIKE_X >= -15 && SPIKE_X <= 25) && ball_y == 27 * vh) {
+		if ((SPIKE_X >= -15 && SPIKE_X <= 20) && ball_y == 27 * vh) {
 
 			show_blast = true;
 			blast_timer = 0;
@@ -82,9 +119,32 @@
 	}
 
 
+	// moving the ball forwards/backwards 
+	void move_ball_backwards(){
+		rotating_angle += 15;
+
+		x_of_play_screen_background += 1;
+		x_of_coin += 1;
+		if (x_of_play_screen_background >= 0) x_of_play_screen_background = -100;
+
+		move_obstacle(1); // 1 means moving RIGHT
+		move_spikes(1); // 1 means moving RIGHT
+		ball_hit_spike();
+	}
+	void move_ball_forwards(){
+		rotating_angle -= 15;
+
+		x_of_play_screen_background -= 1;
+		x_of_coin -= 1;
+		if (x_of_play_screen_background <= -400) x_of_play_screen_background = 0;
+
+		move_obstacle(-1); // -1 means moving LEFT
+		move_spikes(-1); // -1 means moving LEFT
+		ball_hit_spike();
+	}
 
 
-
+	// making pages 
 	void create_landing_page() {
 		iShowImage(0, 0, 100 * vw, 100 * vh, play_screen_background_image);
 
@@ -112,45 +172,22 @@
 
 		if (quit_button_clicked == 1) exit(0);
 	}
-
 	void show_play_screen() {
 		levels_button_clicked = 0;
 		credits_button_clicked = 0;
 		quit_button_clicked = 0;
 
 		//background tiles
-		for (int i = 0; i < 5; i++) {
-			iShowImage((int)(x_of_play_screen_background + i * 100) * vw, 0, 100 * vw, 100 * vh, play_screen_background_image);
-		}
+		make_background();
 
 		// Eda obstacle
+		make_coins();
 		make_obstacles();
 
-
-
-		//	iShowImage(50*vw, 50 * vh, 20 * vw, 20 * vh, obstacle_image_4);
-
-		
-
-
-		//blast before level failed
-		if (show_blast && (blast_timer < blast_duration)) {
-			iShowImage((int)ball_x , (int)ball_y, 25 * vh, 25 * vh, blast_image);
-		}
-		else if (show_blast && blast_timer >= blast_duration) {
-			level_failed = 1;
-			show_blast = false; // prevent looping
-		}
-		else{
-			//draw and rotate ball sprite
-			iRotate((int)(ball_x + 12.5 * vh), (int)(ball_y + 12.5*vh), rotating_angle);
-			iShowImage((int)ball_x, (int)ball_y, 25 * vh, 25 * vh, ball_image);
-			iUnRotate();
-		}
+		make_ball();
 
 		if (level_failed) show_level_failed_screen();
 	}
-
 	void show_levels_screen() {
 		play_button_clicked = 0;
 		quit_button_clicked = 0;
